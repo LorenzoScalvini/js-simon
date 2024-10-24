54250bcb013193c7a6373739d8c5eb7dbc566338
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-10-24 18:38:16 +0200

    ADD: input user

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-    // Funzione per generare un numero intero casuale
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+document.getElementById("input-user").style.display = "none";
+// Funzione per generare un numero intero casuale
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    // Visualizza 5 numeri casuali in orizzontale
-    const numeriDiv = document.getElementById('numeri');
-    for (let i = 0; i < 5; i++) {
-        const numeroCasuale = getRandomInt(1, 100);
-        const numeroElement = document.createElement('div');
-        numeroElement.className = 'alert alert-success numero';
-        numeroElement.textContent = numeroCasuale;
-        numeriDiv.appendChild(numeroElement);
-    }
+// Visualizza 5 numeri casuali in orizzontale
+const numeriDiv = document.getElementById("numeri");
+for (let i = 0; i < 5; i++) {
+  const numeroCasuale = getRandomInt(1, 100);
+  const numeroElement = document.createElement("div");
+  numeroElement.className = "alert alert-success numeri";
+  numeroElement.textContent = numeroCasuale;
+  numeriDiv.appendChild(numeroElement);
+}
 
-    let tempoRimanente = 10;
-    const timerDiv = document.getElementById('timer');
-    //Arrow function per il timer 
-    const timer = setInterval(() => {
-        tempoRimanente--;
-        timerDiv.textContent = `Tempo rimasto per memorizzare: ${tempoRimanente} secondi`;
-        
-        if (tempoRimanente <= 0) {
-            clearInterval(timer);
-            timerDiv.textContent = "Tempo scaduto!";
-            timerDiv.classList.remove('alert-primary', 'alert-danger');
-            timerDiv.classList.add('alert-warning');
-        }
-    }, 1000);
+let tempoRimanente = 10;
+const timerDiv = document.getElementById("timer");
+//Arrow function per il timer
+const timer = setInterval(() => {
+  tempoRimanente--;
+  timerDiv.textContent = `Tempo rimasto per memorizzare: ${tempoRimanente} secondi`;
+
+  if (tempoRimanente <= 0) {
+    clearInterval(timer);
+    document.getElementById("pre-count").style.display = "none";
+    document.getElementById("input-user").style.display = "block";
+  }
+}, 1000);
